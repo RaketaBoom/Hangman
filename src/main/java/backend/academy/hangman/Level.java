c7decd37c1fa04f187293fd4baea0d5925b8991f
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-09-12 11:03:08 +0400

    feat: add enums for level and type of category

--- src/main/java/backend/academy/hangman/Level.java
+++ src/main/java/backend/academy/hangman/Level.java
@@ -1,12 +1,19 @@
 package backend.academy.hangman;
 
 import backend.academy.hangman.exceptions.NonLevelNumberException;
+import lombok.Getter;
 
+@Getter
 public enum Level {
-    EASY,
-    MEDIUM,
-    HARD;
+    EASY("Легко"),
+    MEDIUM("Средне"),
+    HARD("Сложно");
 
+    private String title;
+
+    Level(String title){
+        this.title = title;
+    }
     public static Level fromNumber(int num){
         if(num < 1 || num > Level.values().length){
             throw new NonLevelNumberException();
--- src/main/java/backend/academy/hangman/TypeCategory.java
+++ src/main/java/backend/academy/hangman/TypeCategory.java
@@ -1,13 +1,21 @@
 package backend.academy.hangman;
 
 import backend.academy.hangman.exceptions.NonCategoryNumberException;
+import lombok.Getter;
 
+@Getter
 public enum TypeCategory {
-    ANIMAL,
-    COLOR,
-    COUNTRY,
-    FRUIT,
-    SPORT;
+    ANIMAL("Животные"),
+    COLOR("Цвета"),
+    COUNTRY("Страны"),
+    FRUIT("Фрукты"),
+    SPORT("Спорт");
+
+    private String title;
+
+    TypeCategory(String title){
+        this.title = title;
+    }
 
     public static TypeCategory fromNumber(int num){
         if(num < 1 || num > TypeCategory.values().length){
