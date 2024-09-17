34e6d77e8f2dc92225781db4123875a3a116bab2
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-09-17 17:21:47 +0400

    fix: make Start class non-instantiable

--- src/main/java/backend/academy/hangman/Start.java
+++ src/main/java/backend/academy/hangman/Start.java
@@ -1,6 +1,8 @@
 package backend.academy.hangman;
 
 public class Start {
+    private Start(){}
+
     public static void main(String[] args) {
         Game game = new Game(System.in, System.out);
         game.start();
